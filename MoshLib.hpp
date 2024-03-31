f0631090bffdc022eee007adce2362083692795d
JamahaW <pret2titio1n@gmail.com>
2024-04-01 00:21:05 +0300

    фикс

--- MoshLib.hpp
+++ MoshLib.hpp
@@ -1,28 +1,32 @@
-// Библиотека для робота МОШ (Jamaha)
+// Библиотека для робота МОШ (JamahaW)
 #pragma once
 
-#include "pins.hpp"
 #include "motor.hpp"
+#include "config.hpp"
 
 // Переводной макрос из ММ пути в тики энкодера
-#define MM2TICKS(mm) ( (int32_t)(mm) * 1000L / (int32_t)(pins::MOTOR::MM_IN_1000_TICKS) ) // .. где мой constexpr, Ардуино!!? где С++11???
-
+#define MM2TICKS(mm) ((int32_t)(mm) * 1000L /(int32_t)(pins::MOTOR::MM_IN_1000_TICKS))  // .. где мой constexpr, Ардуино!!? где С++11???
 
 /// @brief проехать 1000 тиков (для определения отношения мм/тик)
 /// @param speed скорость > 0
-void go_1000_ticks(uint8_t speed = pins::MOTOR::DEFAULT_SPEED) { motors.setForTicks(speed, 1000, speed, 1000); }
+void go_1000_ticks(uint8_t speed = pins::MOTOR::DEFAULT_SPEED) {
+  motors.setForTicks(speed, 1000, speed, 1000);
+}
 
 /// @brief Движение по прямой (вперёд или назад)
 /// @param distance_mm дистанция в мм (<0 для движения назад)
 /// @param speed скорость > 0
 void goDirect(int32_t distance_mm, uint8_t speed = pins::MOTOR::DEFAULT_SPEED) {
-    int32_t ticks = MM2TICKS(distance_mm);
-    motors.setForTicks(speed, ticks, speed, ticks);
+  int32_t ticks = MM2TICKS(distance_mm);
+  motors.setForTicks(speed, ticks, speed, ticks);
 }
 
-/// @brief поворот по часовой стрелке на a градусов вокруг центра
-/// @param a угол поворота
+/**
+ * @brief поворот по часовой стрелке на a градусов вокруг центра
+ * @param a угол поворота
+ * @param speed
+ */
 void turnAngle(int16_t a, uint8_t speed = pins::MOTOR::DEFAULT_SPEED) {
-    int32_t ticks = MM2TICKS((int32_t)a * pins::MOTOR::TRACK * M_PI / 360);
-    motors.setForTicks(speed, ticks, speed, -ticks);
+  int32_t ticks = MM2TICKS((int32_t)a * pins::MOTOR::TRACK * M_PI / 360);
+  motors.setForTicks(speed, ticks, speed, -ticks);
 }
--- linesensor.hpp
+++ linesensor.hpp
@@ -7,7 +7,15 @@
 
 /// @brief Аналоговый Датчик линии
 class LineSensor {
+    private:
+    
     const uint8_t PIN;
-    const uint16_t on_line;
+    const uint16_t ON_LINE;
     const uint16_t on_field;
+
+    public:
+
+    LineSensor(
+        const uint8_t pin,
+    )
 };
